bdd945816060db12bf6204eafb3a9e95ccc6b79e
Roy Schonberg <schonberg.r@gmail.com>
2024-11-17 12:48:54 +0200

    use testing-library

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,11 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 
 // Note: import explicitly to use the types shipped with jest.
 import { it } from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react-native';
 import App from '../src/App';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  render(<App />);
 });
